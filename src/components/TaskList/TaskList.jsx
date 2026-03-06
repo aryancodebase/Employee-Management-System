8aca5829762ffc53a25d3a5bf6c60b1b1bc86fca
Aryan <aryan.gupta158935@gmail.com>
2026-03-06 11:14:37 +0530

    EMS || fetched task list for employees

--- src/components/TaskList/TaskList.jsx
+++ src/components/TaskList/TaskList.jsx
@@ -1,150 +1,38 @@
+import { useContext } from "react";
+import { AuthContext } from "../../context/AuthProvider";
+
 const TaskList = () => {
+  const { user } = useContext(AuthContext);
+
   return (
     <div className="h-[50%] w-full">
       <div
         className="p-10 flex gap-4 overflow-x-auto flex-nowrap h-full"
         id="tasklist"
       >
-        <div className="bg-red-300 w-[20%] h-full rounded-lg shrink-0">
-          <div className="flex justify-between items-center p-4">
-            <h1 className="bg-red-500 p-2 rounded text-sm">High</h1>
-            <h2 className="font-semibold text-sm">03 March 2026</h2>
+        {user?.tasks?.map((task) => (
+          <div
+            key={task.id}
+            className="bg-red-300 w-[20%] h-full rounded-lg shrink-0"
+          >
+            <div className="flex justify-between items-center p-4">
+              <h1 className="bg-red-500 p-2 rounded text-sm">
+                {task.category}
+              </h1>
+              <h2 className="font-semibold text-sm">{task.date}</h2>
+            </div>
+
+            <div className="p-4">
+              <h1 className="font-semibold text-xl">{task.title}</h1>
+
+              <p className="text-sm mt-5">{task.description}</p>
+
+              <p className="mt-3 text-xs font-semibold">
+                Status: {task.status}
+              </p>
+            </div>
           </div>
-          <div className="p-4">
-            <h1 className="font-semibold text-xl">Make a Youtube Video</h1>
-            <p className="text-sm mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              similique voluptates sequi unde perspiciatis iusto sunt, illum
-              doloribus voluptatem odit.
-            </p>
-          </div>
-        </div>
-        <div className="bg-red-300 w-[20%] h-full rounded-lg shrink-0">
-          <div className="flex justify-between items-center p-4">
-            <h1 className="bg-red-500 p-2 rounded text-sm">High</h1>
-            <h2 className="font-semibold text-sm">03 March 2026</h2>
-          </div>
-          <div className="p-4">
-            <h1 className="font-semibold text-xl">Make a Youtube Video</h1>
-            <p className="text-sm mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              similique voluptates sequi unde perspiciatis iusto sunt, illum
-              doloribus voluptatem odit.
-            </p>
-          </div>
-        </div>
-        <div className="bg-red-300 w-[20%] h-full rounded-lg shrink-0">
-          <div className="flex justify-between items-center p-4">
-            <h1 className="bg-red-500 p-2 rounded text-sm">High</h1>
-            <h2 className="font-semibold text-sm">03 March 2026</h2>
-          </div>
-          <div className="p-4">
-            <h1 className="font-semibold text-xl">Make a Youtube Video</h1>
-            <p className="text-sm mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              similique voluptates sequi unde perspiciatis iusto sunt, illum
-              doloribus voluptatem odit.
-            </p>
-          </div>
-        </div>
-        <div className="bg-red-300 w-[20%] h-full rounded-lg shrink-0">
-          <div className="flex justify-between items-center p-4">
-            <h1 className="bg-red-500 p-2 rounded text-sm">High</h1>
-            <h2 className="font-semibold text-sm">03 March 2026</h2>
-          </div>
-          <div className="p-4">
-            <h1 className="font-semibold text-xl">Make a Youtube Video</h1>
-            <p className="text-sm mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              similique voluptates sequi unde perspiciatis iusto sunt, illum
-              doloribus voluptatem odit.
-            </p>
-          </div>
-        </div>
-        <div className="bg-red-300 w-[20%] h-full rounded-lg shrink-0">
-          <div className="flex justify-between items-center p-4">
-            <h1 className="bg-red-500 p-2 rounded text-sm">High</h1>
-            <h2 className="font-semibold text-sm">03 March 2026</h2>
-          </div>
-          <div className="p-4">
-            <h1 className="font-semibold text-xl">Make a Youtube Video</h1>
-            <p className="text-sm mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              similique voluptates sequi unde perspiciatis iusto sunt, illum
-              doloribus voluptatem odit.
-            </p>
-          </div>
-        </div>
-        <div className="bg-red-300 w-[20%] h-full rounded-lg shrink-0">
-          <div className="flex justify-between items-center p-4">
-            <h1 className="bg-red-500 p-2 rounded text-sm">High</h1>
-            <h2 className="font-semibold text-sm">03 March 2026</h2>
-          </div>
-          <div className="p-4">
-            <h1 className="font-semibold text-xl">Make a Youtube Video</h1>
-            <p className="text-sm mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              similique voluptates sequi unde perspiciatis iusto sunt, illum
-              doloribus voluptatem odit.
-            </p>
-          </div>
-        </div>
-        <div className="bg-red-300 w-[20%] h-full rounded-lg shrink-0">
-          <div className="flex justify-between items-center p-4">
-            <h1 className="bg-red-500 p-2 rounded text-sm">High</h1>
-            <h2 className="font-semibold text-sm">03 March 2026</h2>
-          </div>
-          <div className="p-4">
-            <h1 className="font-semibold text-xl">Make a Youtube Video</h1>
-            <p className="text-sm mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              similique voluptates sequi unde perspiciatis iusto sunt, illum
-              doloribus voluptatem odit.
-            </p>
-          </div>
-        </div>
-        <div className="bg-red-300 w-[20%] h-full rounded-lg shrink-0">
-          <div className="flex justify-between items-center p-4">
-            <h1 className="bg-red-500 p-2 rounded text-sm">High</h1>
-            <h2 className="font-semibold text-sm">03 March 2026</h2>
-          </div>
-          <div className="p-4">
-            <h1 className="font-semibold text-xl">Make a Youtube Video</h1>
-            <p className="text-sm mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              similique voluptates sequi unde perspiciatis iusto sunt, illum
-              doloribus voluptatem odit.
-            </p>
-          </div>
-        </div>
-        <div className="bg-red-300 w-[20%] h-full rounded-lg shrink-0">
-          <div className="flex justify-between items-center p-4">
-            <h1 className="bg-red-500 p-2 rounded text-sm">High</h1>
-            <h2 className="font-semibold text-sm">03 March 2026</h2>
-          </div>
-          <div className="p-4">
-            <h1 className="font-semibold text-xl">Make a Youtube Video</h1>
-            <p className="text-sm mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              similique voluptates sequi unde perspiciatis iusto sunt, illum
-              doloribus voluptatem odit.
-            </p>
-          </div>
-        </div>
-        <div className="bg-red-300 w-[20%] h-full rounded-lg shrink-0">
-          <div className="flex justify-between items-center p-4">
-            <h1 className="bg-red-500 p-2 rounded text-sm">High</h1>
-            <h2 className="font-semibold text-sm">03 March 2026</h2>
-          </div>
-          <div className="p-4">
-            <h1 className="font-semibold text-xl">Make a Youtube Video</h1>
-            <p className="text-sm mt-5">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
-              similique voluptates sequi unde perspiciatis iusto sunt, illum
-              doloribus voluptatem odit.
-            </p>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
